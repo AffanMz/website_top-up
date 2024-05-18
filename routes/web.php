e94b2a81c54cca10b3a733c7508b5270e18b02dd
AffanMz <affansukses99@gmail.com>
2024-05-18 13:10:05 +0700

    config Laravel to Tailwind

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landingPage');
+});
+
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/order', function () {
+    return view('order');
+});
+
+Route::get('/order-ent', function () {
+    return view('order-ent');
+});
+
+Route::get('/membership', function () {
+    return view('membership');
 });
